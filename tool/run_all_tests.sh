4a74ae7e7d8aeadd755da5a7afbcc2a4bcf06dda
Paul Brauner <polux2001@gmail.com>
2013-05-30 01:13:49 +0200

    analyze more

--- tool/run_all_tests.sh
+++ tool/run_all_tests.sh
@@ -3,6 +3,8 @@
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 dartanalyzer $ROOT_DIR/lib/*.dart \
+&& dartanalyzer $ROOT_DIR/test/*.dart \
+&& dartanalyzer $ROOT_DIR/example/*.dart \
 && dart --enable-checked-mode $ROOT_DIR/example/simple_arith.dart \
 && dart --enable-checked-mode $ROOT_DIR/example/full_arith.dart \
 && dart --enable-checked-mode $ROOT_DIR/test/parsers_test.dart

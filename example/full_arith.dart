7ba818d528f731d72da8e8ed7f48b6216011556e
devkabiir <dev@kabiir.me>
2019-04-24 21:53:16 +0530

    ♻️refactor (example) Fix `full_arith` example

--- example/full_arith.dart
+++ example/full_arith.dart
@@ -1,7 +1,9 @@
 // Copyright (c) 2012, Google Inc. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-// Author: Paul Brauner (polux@google.com)
+// Authors:
+//   Paul Brauner (polux@google.com)
+//   Dinesh Ahuja (dev@kabiir.me)
 
 library example;
 
@@ -11,33 +13,34 @@ import 'package:parsers/parsers.dart';
 // helps handling infix operators with the same precedence.
 
 class Arith {
-  digits2int(digits) => int.parse(digits.join());
+  int digits2int(List<String> digits) => int.parse(digits.join());
 
-  lexeme(parser) => parser < spaces;
-  token(str) => lexeme(string(str));
-  parens(parser) => parser.between(token('('), token(')'));
+  Parser<A> lexeme<A>(Parser<A> parser) => parser < spaces;
+  Parser<String> token(String str) => lexeme(string(str));
+  Parser<A> parens<A>(Parser<A> parser) =>
+      parser.between(token('('), token(')'));
 
-  get start => expr() < eof;
+  Parser get start => expr() < eof;
 
-  get comma => token(',');
-  get times => token('*');
-  get div => token('~/');
-  get plus => token('+');
-  get minus => token('-');
-  get number => lexeme(digit.many1) ^ digits2int;
+  Parser get comma => token(',');
+  Parser get times => token('*');
+  Parser get div => token('~/');
+  Parser get plus => token('+');
+  Parser get minus => token('-');
+  Parser get number => (lexeme(digit.many1) ^ digits2int);
 
-  expr() => rec(term).chainl1(addop);
-  term() => rec(atom).chainl1(mulop);
-  atom() => number | parens(rec(expr));
+  Parser expr() => rec(term).chainl1(addop);
+  Parser term() => rec(atom).chainl1(mulop);
+  Parser atom() => number | parens(rec(expr));
 
-  get addop =>
-      (plus > success((x, y) => x + y)) | (minus > success((x, y) => x - y));
+  Parser<Function> get addop => (plus.thenKeep(success((x, y) => x + y)))
+      .or(minus.thenKeep(success((x, y) => x - y)));
 
-  get mulop =>
-      (times > success((x, y) => x * y)) | (div > success((x, y) => x ~/ y));
+  Parser<Function> get mulop => (times.thenKeep(success((x, y) => x * y)))
+      .or(div.thenKeep(success((x, y) => x ~/ y)));
 }
 
 main() {
-  final s = "1 * 2 ~/ 2 + 3 * (4 + 5 - 1)";
+  const s = '1 * 2 ~/ 2 + 3 * (4 + 5 - 1)';
   print(Arith().start.parse(s)); // prints 25
 }
